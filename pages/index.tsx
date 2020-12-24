3f6aea40b066918fbcb3b8205675c842f5440151
kastin <kastin@kastin.pl>
2020-12-24 17:52:42 +0100

    yarn create next-app touch tsconfig.json yarn add --dev typescript @types/react @types/node

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      react-next-demo
+    </div>
+  )
+}
+
+export default Home
