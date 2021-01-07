b5cefcbf672367851661c33801c4da3beab3c77a
kastin <kastin@kastin.pl>
2021-01-07 12:50:26 +0100

    favicon ico SVG approach

--- src/ui/BaseLayout/BaseLayout.tsx
+++ src/ui/BaseLayout/BaseLayout.tsx
@@ -1,13 +1,15 @@
+import { useTheme } from '@material-ui/core'
 import Head from 'next/head'
 import React from 'react'
 import { BaseLayoutProps } from './types'
 
 export const BaseLayout: React.FC<BaseLayoutProps> = ({ title, children }) => {
+  const theme = useTheme()
+
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
         <title>{title}</title>
       </Head>
       {children}
